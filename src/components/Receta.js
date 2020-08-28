bba69bfe4a97832c3cbcb0f8cc8b993b6df90772
David Garcia <dgarciabravo13@gmail.com>
2020-08-28 16:44:08 +0200

    change title in the card

--- src/components/Receta.js
+++ src/components/Receta.js
@@ -60,7 +60,7 @@ const Receta = ({ receta }) => {
   return (
     <div className="col-md-4 mb-3">
       <div className="card">
-        <h2 className="card-header">{receta.strDrink}</h2>
+        <h6 className="card-header">{receta.strDrink}</h6>
         <img
           className="card-img-top"
           src={receta.strDrinkThumb}
